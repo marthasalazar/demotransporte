3ca88744a672c40c9f4499c3852ef1cf9b54180e
Martha Salazar <marthavsalazar@mail.com>
2020-10-05 14:46:47 -0600

    AVANCE MENUS LISTOS.

---
+++ app/src/main/java/com/example/prueba/mensajesinfo.java
@@ -0,0 +1,13 @@
+package com.example.prueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mensajesinfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mensajesinfo);
+    }
+}
