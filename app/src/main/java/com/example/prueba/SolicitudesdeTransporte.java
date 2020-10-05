97f694cb4724c9eb08304e97903be0e993604dad
Martha Salazar <marthavsalazar@mail.com>
2020-10-05 11:49:22 -0600

    Commit inicial

---
+++ app/src/main/java/com/example/prueba/SolicitudesdeTransporte.java
@@ -0,0 +1,13 @@
+package com.example.prueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SolicitudesdeTransporte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitudesde_transporte);
+    }
+}
---
+++ app/src/main/java/com/example/prueba/aprobacionydenegacionsolicitudes.java
@@ -0,0 +1,15 @@
+package com.example.prueba;
+
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aprobacionydenegacionsolicitudes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aprobacionydenegacionsolicitudes);
+    }
+}
